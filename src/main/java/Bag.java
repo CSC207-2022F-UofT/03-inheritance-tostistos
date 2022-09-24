5f879362d339b6dddbccceee774ddef5e17780bd
tostistos <fidler.patrick@gmail.com>
2022-09-24 19:01:57 -0400

    Finished TODOs in Bag.java, HandBag.java, and BagMain.java. Implemented CrossbodyBag.java.

--- src/main/java/Bag.java
+++ src/main/java/Bag.java
@@ -13,9 +13,10 @@ public abstract class Bag {
      *       - an int named capacity
      *       - an array of Strings named contents
      */
-
-
-
+    private String color;
+    private int numberOfContents;
+    private int capacity;
+    private String[] contents;
 
     /*
      * TODO: Create a constructor that takes two arguments:
@@ -26,9 +27,12 @@ public abstract class Bag {
      * be empty (e.g. numberOfContents is 0 and an empty String array for
      * its contents.)
      */
-
-
-
+    public Bag(String bag_colour, int bag_capacity){
+        this.color = bag_colour;
+        this.capacity = bag_capacity;
+        this.numberOfContents = 0;
+        this.contents = new String[capacity];
+    }
 
     /*
      * TODO: Create a variety of 'getter' functions.
@@ -37,18 +41,25 @@ public abstract class Bag {
      *           - getNumberOfContents
      *           - getCapacity
      */
+    public String getColor(){
+        return this.color;
+    }
 
+    public int getNumberOfContents(){
+        return this.numberOfContents;
+    }
 
-
+    public int getCapacity(){
+        return this.capacity;
+    }
 
     /*
      * TODO: Create a setter function called setColor which sets the
      *       color of this bag to the given color.
      */
-
-
-
-
+    public void setColor(String to_set){
+        this.color = to_set;
+    }
 
     /*
      * TODO: Create a method called addItem that takes in a String
@@ -60,10 +71,14 @@ public abstract class Bag {
      *       This method should return true if the item was added
      *       and false otherwise.
      */
-
-
-
-
+    public boolean addItem(String bagged_item){
+        if (this.numberOfContents < this.capacity){
+            this.contents[this.numberOfContents] = bagged_item;
+            this.numberOfContents++;
+            return true;
+        }
+        return false;
+    }
 
     /**
      * TODO: Create a method called popItem that returns a String.
@@ -75,10 +90,16 @@ public abstract class Bag {
      *
      * @return
      */
-
-
-
-
+    public String popItem(){
+        if (this.numberOfContents == 0){
+            return null;
+        } else {
+            String popped_item = this.contents[this.numberOfContents - 1];
+            this.contents[this.numberOfContents - 1] = null;
+            this.numberOfContents--;
+            return popped_item;
+        }
+    }
 
     /**
      * Increase this bag's capacity by n.
@@ -86,8 +107,12 @@ public abstract class Bag {
      * @param n the amount to increase this Bag's capacity by
      */
     public void increaseCapacity(int n) {
-        // TODO: Implement this method.
-
+        String[] new_contents = new String[this.capacity + n];
+        for (int i = 0; i < this.capacity; i++){
+            new_contents[i] = this.contents[i];
+        }
+        this.contents = new_contents;
+        this.capacity = this.capacity + n;
     }
 
     /**
--- src/main/java/BagMain.java
+++ src/main/java/BagMain.java
@@ -15,7 +15,12 @@ class BagMain {
      * on new Bag types (and HandBag subclasses)!
      */
     public static void enhanceBags(Bag[] bags, boolean double_enhance_handbags) {
-        // TODO: Implement this.
+        for (Bag bag : bags){
+            bag.enhance();
+            if (double_enhance_handbags && bag instanceof HandBag) {
+                bag.enhance();
+            }
+        }
     }
 
     /**
@@ -28,6 +33,12 @@ class BagMain {
      * @return The total number of straps of CrossbodyBags.
      */
     public static int countCrossbodyStraps(Bag[] bags) {
-        // TODO: Implement this.
+        int strap_count = 0;
+        for (Bag bag : bags) {
+            if (bag instanceof CrossbodyBag) {
+                strap_count = strap_count + ((CrossbodyBag) bag).getNumberOfStraps();
+            }
+        }
+        return strap_count;
     }
 }
